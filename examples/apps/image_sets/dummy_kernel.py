b960bb11a160375f1722ed12a0b90b74cb6bda87
Konstantinos Rematas <krematas@cs.washington.edu>
2018-05-11 16:48:14 -0700

    working with images now
    Signed-off-by: Konstantinos Rematas <krematas@cs.washington.edu>

--- examples/apps/image_sets/dummy_kernel.py
+++ examples/apps/image_sets/dummy_kernel.py
@@ -1,6 +1,6 @@
 import scannerpy
 import cv2
-
+import numpy as np
 
 class MyDummyKernel(scannerpy.Kernel):
     def __init__(self, config, protobufs):
@@ -8,7 +8,7 @@ class MyDummyKernel(scannerpy.Kernel):
         self._h = config.args['h']
 
     def execute(self, columns):
-        return [cv2.resize(columns[0], (self._w, self._h))]
+        return [cv2.resize((columns[0]*(columns[1]/255.)).astype(np.uint8), (self._w, self._h))]
 
 
 KERNEL = MyDummyKernel
--- examples/apps/image_sets/main.py
+++ examples/apps/image_sets/main.py
@@ -7,31 +7,32 @@ def b(s):
     return bytes(s, 'utf-8')
 
 
-db = Database()
-
-img1 = cv2.imread('/home/krematas/Mountpoints/grail/data/barcelona/players/images/00114_00010.jpg')
-img2 = cv2.imread('/home/krematas/Mountpoints/grail/data/barcelona/players/images/00114_00008.jpg')
 cwd = '/home/krematas/code/scanner/examples/apps/image_sets'
 
+db = Database()
 
-db.new_table('mine', ['index', 'image'], [[b('00114_00010'), img1.tobytes()], [b('00114_00008'), img2.tobytes()]], force=True)
-input_table = db.table('mine')
 
-db.register_op('ProcessImage', [('image', ColumnType.Video)], [('resized', ColumnType.Video)])
+encoded_image = db.sources.Files()
+frame = db.ops.ImageDecoder(img=encoded_image)
+
+encoded_mask = db.sources.Files()
+frame2 = db.ops.ImageDecoder(img=encoded_mask)
+
+db.register_op('ProcessImage', [('image', ColumnType.Video), ('mask', ColumnType.Video)], [('resized', ColumnType.Video)])
 db.register_python_kernel('ProcessImage', DeviceType.CPU, cwd + '/dummy_kernel.py')
 
-
-frame = db.sources.FrameColumn()
-
-
-# Then we use our op just like in the other examples.
-resized = db.ops.ProcessImage(frame=frame, w=60, h=60)
+resized = db.ops.ProcessImage(image=frame, mask=frame2,  w=60, h=60)
 output_op = db.sinks.FrameColumn(columns={'frame': resized})
 
-job = Job(op_args={
-    frame: input_table.column('image'),
-    output_op: 'example_resized',
-})
+job = Job(
+    op_args={
+        encoded_image: {'paths': ['/home/krematas/Mountpoints/grail/data/barcelona/players/images/00114_00010.jpg',
+                                  '/home/krematas/Mountpoints/grail/data/barcelona/players/images/00114_00008.jpg']},
+        encoded_mask: {'paths': ['/home/krematas/Mountpoints/grail/data/barcelona/players/masks/00114_00010.png',
+                                  '/home/krematas/Mountpoints/grail/data/barcelona/players/masks/00114_00008.png']},
+
+        output_op: 'example_resized',
+    })
 [out_table] = db.run(output_op, [job], force=True)
 out_table.column('frame').save_mp4('haha')
 
